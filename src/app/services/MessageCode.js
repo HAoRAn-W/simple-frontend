9184784d96849a7ed750cfd328ffc12ef0ba904d
Haoran Wang <haoran2326@gmail.com>
2023-07-18 23:07:05 -0400

    add new codes

--- src/app/services/MessageCode.js
+++ src/app/services/MessageCode.js
@@ -2,6 +2,12 @@ export const INIT_CODE = 0;
 
 export const UNDEFINED_ERROR = 1;
 
+export const NEED_REFRESH = 2;
+
+export const NEED_RELOGIN = 3;
+
+export const UNAUTHORIZED = 4;
+
 export const USER_EXIST = 100;
 
 export const EMAIL_EXIST = 101;
@@ -17,4 +23,6 @@ export const LOGGED_OUT = 201;
 export const REFRESH_TOKEN_EXPIRED = 202;
 
 export const ACCESS_TOKEN_RENEWED = 203;
+
+
 
