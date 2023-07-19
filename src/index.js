32332c4d90e50896a9a2d87a80b4932ef6703e8d
Haoran Wang <haoran2326@gmail.com>
2023-07-18 23:06:57 -0400

    delete React.StrictMode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>
 );
