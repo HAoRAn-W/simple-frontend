a953449b0c60dd9e3f1be71a6eb2c093d5976bb4
Haoran Wang <haoran2326@gmail.com>
2023-07-18 23:06:40 -0400

    add interceptor to refresh token

--- src/app/services/refresh.service.js
+++ src/app/services/refresh.service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { UNAUTHORIZED, UNDEFINED_ERROR } from "./MessageCode";
 
 const refreshClient = axios.create({
-  baseURL: "http://localhost:8080/api/refresh/",
+  baseURL: "http://localhost:8080/api/refresh",
   withCredentials: true,
 });
 
@@ -9,8 +10,10 @@ const refreshClient = axios.create({
 // if access token expired, a 401 error will be raised by other requests.
 // call refreshToken to request a new access token
 const refreshToken = () => {
-    return refreshClient.get().then((response) => {
+    return refreshClient.post().then((response) => {
         return response.data;
+    }).catch(error => {
+        return {code: UNAUTHORIZED, message:error.toString()};
     })
 }
 
--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -1,9 +1,59 @@
 import axios from "axios";
+import RefreshService from "./refresh.service";
+import {
+  ACCESS_TOKEN_RENEWED,
+  NEED_RELOGIN,
+  REFRESH_TOKEN_EXPIRED,
+  UNAUTHORIZED,
+  UNDEFINED_ERROR,
+} from "./MessageCode";
 
 const userClient = axios.create({
   baseURL: "http://localhost:8080/api/test/",
   withCredentials: true,
 });
+
+class LoginRequiredError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "LoginRequiredError";
+    this.code = NEED_RELOGIN;
+  }
+}
+
+class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UnauthorizedError";
+    this.code = UNAUTHORIZED;
+  }
+}
+
+userClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const request = error.config;
+    if (error.response.status === 401) {
+      if (!request._retry) {
+        request._retry = true;
+        return RefreshService.refreshToken().then((data) => {
+          if (data.code === ACCESS_TOKEN_RENEWED) {
+            console.log('retry success')
+            return userClient(request);
+          } else if (data.code === REFRESH_TOKEN_EXPIRED) {
+            console.log('need login')
+            throw new LoginRequiredError("Login is required");
+          } else {
+            throw new UnauthorizedError("Unauthorized");
+          }
+        });
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 const getPublicConetnt = () => {
   return userClient.get("all");
@@ -11,7 +61,17 @@ const getPublicConetnt = () => {
 
 const getUserContent = () => {
   return userClient.get("user").then((response) => {
+    console.log('user:')
+    console.log(response.data);
     return response.data;
+  }).catch((error) => {
+    if (error instanceof LoginRequiredError) {
+      return {code: NEED_RELOGIN, message: 'Log in is required'}
+    } else if (error instanceof UnauthorizedError ) {
+      return {code: UNAUTHORIZED, message: 'unauthorized'}
+    } else {
+      return {code: UNDEFINED_ERROR, message: error.toString()}
+    }
   });
 };
 
