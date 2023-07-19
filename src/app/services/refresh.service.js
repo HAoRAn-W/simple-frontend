14d51184b8dd39870f395466b00b87322631a009
Haoran Wang <haoran2326@gmail.com>
2023-07-19 15:41:10 -0400

    refresh service code reformat

--- src/app/services/refresh.service.js
+++ src/app/services/refresh.service.js
@@ -1,24 +1,28 @@
 import axios from "axios";
-import { UNAUTHORIZED, UNDEFINED_ERROR } from "./MessageCode";
+import { UNAUTHORIZED } from "./MessageCode";
 
 const refreshClient = axios.create({
   baseURL: "http://localhost:8080/api/refresh",
   withCredentials: true,
 });
 
-
 // if access token expired, a 401 error will be raised by other requests.
 // call refreshToken to request a new access token
 const refreshToken = () => {
-    return refreshClient.post().then((response) => {
-        return response.data;
-    }).catch(error => {
-        return {code: UNAUTHORIZED, message:error.toString()};
+  return refreshClient
+    .post()
+    .then((response) => {
+      console.log("refreshToken() response data:");
+      console.log(response.data);
+      return response.data;
     })
-}
+    .catch((error) => {
+      return { code: UNAUTHORIZED, message: error.toString() };
+    });
+};
 
 const RefreshService = {
-    refreshToken,
-}
+  refreshToken,
+};
 
 export default RefreshService;
