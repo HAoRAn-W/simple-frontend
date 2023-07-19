15feddf5d8373d02af02df6e7206fe219f06cbfb
Haoran Wang <haoran2326@gmail.com>
2023-07-19 16:09:18 -0400

    update post list so it can shows real page list

--- src/components/home/PostInfo.js
+++ src/components/home/PostInfo.js
@@ -1,40 +1,30 @@
-import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardMedia, Container, Typography } from "@mui/material";
 import React from "react";
 
 // Basic post info showed on home page
-function PostInfo() {
+function PostInfo({ post }) {
+  const date = new Date(post.createdTime);
 
   return (
-
-      <Card sx={{ display: "flex"}} >
-        <CardMedia
-          component="img"
-          sx={{ width: 200 }}
-          image="https://source.unsplash.com/random?wallpapers"
-          alt="Cover picture"
-        />
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <CardContent sx={{ flex: "1 0 auto" }}>
-            <Typography variant="h5">Post Title</Typography>
-            <DataLine />
-            <Typography>
-              Post Introduction, this is a post intropuction: today I go out ith
-              my girlfriend and we eat hot pot and watch movie. I love my
-              girlfriend
-            </Typography>
-          </CardContent>
-        </Box>
-      </Card>
+    <Card sx={{ display: "flex", width: "100%" }}>
+      <CardMedia
+        component="img"
+        sx={{ width: 200 }}
+        image="https://source.unsplash.com/random?wallpapers"
+        alt="Cover picture"
+      />
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography variant="h5">{post.title}</Typography>
+          <Container sx={{ display: "flex", justifyContent: "space-between"}}>
+            <Typography>{`${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`}</Typography>
+            <Typography> {post.category.name} </Typography>
+          </Container>
+          <Typography>{post.description}</Typography>
+        </CardContent>
+      </Box>
+    </Card>
   );
 }
 
-const DataLine = () => {
-  return (
-    <Box sx={{ display: "flex" }}>
-      <Typography>Jul 18 </Typography>
-      <Typography> Java </Typography>
-      <Typography> Views: 123 </Typography>
-    </Box>
-  );
-};
 export default PostInfo;
--- src/components/home/PostList.js
+++ src/components/home/PostList.js
@@ -2,30 +2,32 @@ import { Container, List, ListItemButton } from "@mui/material";
 import React from "react";
 import PostInfo from "./PostInfo";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function PostList() {
+  const posts = useSelector((state) => state.page.posts);
   return (
     <Container
       style={{
         display: "flex",
-        flexDirection: 'column',
-        alignItems: 'flex-end'
+        flexDirection: "column",
+        alignItems: "flex-end",
       }}
     >
-      {/* <Typography variant="h4" marginTop={'20px'}>Posts</Typography> */}
       <List sx={{ width: "90%" }} disablePadding>
-        <ListItemButton style={{ marginTop: "15px" }} component={Link} to='/1'>
-          <PostInfo />
-        </ListItemButton>
-        <ListItemButton style={{ marginTop: "15px" }}>
-          <PostInfo />
-        </ListItemButton>
-        <ListItemButton style={{ marginTop: "15px" }}>
-          <PostInfo />
-        </ListItemButton>
-        <ListItemButton style={{ marginTop: "15px" }}>
-          <PostInfo />
-        </ListItemButton>
+        {posts.map((post) => {
+          return (
+            <ListItemButton
+              style={{ marginTop: "15px"}}
+              component={Link}
+              to={`/${post.id}`}
+              key={post.id}
+            >
+              <PostInfo post={post}/>
+            </ListItemButton>
+          );
+        })}
+
       </List>
     </Container>
   );
