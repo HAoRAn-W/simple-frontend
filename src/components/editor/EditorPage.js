4d60c45fbba494fd767fc6320f5e5a81da78ed3e
Haoran Wang <haoran2326@gmail.com>
2023-07-20 12:25:21 -0400

    modified Editor page

--- src/components/editor/EditorPage.js
+++ src/components/editor/EditorPage.js
@@ -1,6 +1,14 @@
-import { Container, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import {
+  Container,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useState } from "react";
+import PostList from "../home/PostList";
+import PaginationBar from "../PaginationBar";
+import CategoryPage from '../category/CategoryPage';
 
 function EditorPage() {
   const [section, setSection] = useState("posts");
@@ -14,20 +22,35 @@ function EditorPage() {
   };
   return (
     <>
-    <Container sx={{display: 'flex', flexDirection:'column', alignItems: 'center'}}>
-    <ToggleButtonGroup {...control} sx={{marginTop: 6}}>
-        <ToggleButton value={"posts"} key="posts">
-          <Typography>post</Typography>
-        </ToggleButton>
-        <ToggleButton value={"categories"} key="categories">
-          <Typography>categories</Typography>
-        </ToggleButton>
-        <ToggleButton value={"tags"} key="tags">
-          <Typography>tags</Typography>
-        </ToggleButton>
-      </ToggleButtonGroup>
-    </Container>
-      
+      <Container
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <ToggleButtonGroup {...control} sx={{ marginTop: 6 }}>
+          <ToggleButton value={"posts"} key="posts">
+            <Typography>post</Typography>
+          </ToggleButton>
+          <ToggleButton value={"categories"} key="categories">
+            <Typography>categories</Typography>
+          </ToggleButton>
+          <ToggleButton value={"tags"} key="tags">
+            <Typography>tags</Typography>
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <div>
+          {section === "posts" ? (
+            <>
+              <div style={{ height: "100vh" }}>
+                <PostList />
+              </div>
+              <PaginationBar />
+            </>
+          ) : section === "categories" ? (
+            <div><CategoryPage/></div>
+          ) : (
+            <div>tags</div>
+          )}
+        </div>
+      </Container>
     </>
   );
 }
