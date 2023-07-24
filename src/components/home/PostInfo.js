a89781377f39aa3974269be693f9dda6fb742c66
Haoran Wang <haoran2326@gmail.com>
2023-07-23 21:21:58 -0400

    bug fix, getMonth() output ranges from 0 to 11

--- src/components/home/PostInfo.js
+++ src/components/home/PostInfo.js
@@ -17,7 +17,7 @@ function PostInfo({ post }) {
         <CardContent sx={{ flex: "1 0 auto" }}>
           <Typography variant="h5">{post.title}</Typography>
           <Container sx={{ display: "flex", justifyContent: "space-between"}}>
-            <Typography>{`${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`}</Typography>
+            <Typography>{`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`}</Typography>
             <Typography> {post.category.name} </Typography>
           </Container>
           <Typography>{post.description}</Typography>
