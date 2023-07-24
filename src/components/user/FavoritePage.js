62682ec67f8c8cff3cfad65f22ff86e13eb61b43
Haoran Wang <haoran2326@gmail.com>
2023-07-23 22:40:34 -0400

    display user favorites

--- src/components/user/FavoritePage.js
+++ src/components/user/FavoritePage.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import UserServcice from "../../app/services/user.service";
+import { SUCCESSFUL } from "../../app/constants/MessageCode";
+import { ListItemButton } from "@mui/material";
+import PostInfo from "../home/PostInfo";
+import { useNavigate } from "react-router-dom";
 
 function FavoritePage() {
+  const [favorites, setFavorites] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    UserServcice.getFavoriteList().then((data) => {
+      if (data.code === SUCCESSFUL) {
+        setFavorites(data.favorites);
+      }
+    });
+  }, []);
   return (
     <div>
-      FavoritePage
+      {favorites.map((post) => (
+        <ListItemButton
+          style={{ marginTop: "15px" }}
+          key={post.id}
+          onClick={() => {
+            navigate(`/${post.id}`);
+          }}
+        >
+          <PostInfo post={post} />
+        </ListItemButton>
+      ))}
     </div>
-  )
+  );
 }
 
-export default FavoritePage
+export default FavoritePage;
