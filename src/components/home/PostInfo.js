6d3fa3a812319063a2eda2afd704ac755019e238
Haoran Wang <haoran2326@gmail.com>
2023-07-24 15:04:00 -0400

    styled post info

--- src/components/home/PostInfo.js
+++ src/components/home/PostInfo.js
@@ -1,4 +1,12 @@
-import { Box, Card, CardContent, CardMedia, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
 import React from "react";
 
 // Basic post info showed on home page
@@ -9,18 +17,42 @@ function PostInfo({ post }) {
     <Card sx={{ display: "flex", width: "100%" }}>
       <CardMedia
         component="img"
-        sx={{ width: 200 }}
-        image="https://source.unsplash.com/random?wallpapers"
+        sx={{ width: 400 }}
+        image={post.coverUrl}
         alt="Cover picture"
       />
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography variant="h5">{post.title}</Typography>
-          <Container sx={{ display: "flex", justifyContent: "space-between"}}>
-            <Typography>{`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`}</Typography>
-            <Typography> {post.category.name} </Typography>
-          </Container>
-          <Typography>{post.description}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+        }}
+      >
+        <CardContent>
+          <Typography variant="h5" fontFamily={"Roboto"} gutterBottom>
+            {post.title}
+          </Typography>
+          <div style={{ display: "flex", paddingLeft: "0px" }}>
+            <div
+              style={{ display: "flex", flexDirection: "row", paddingLeft: "0px" }}
+            >
+              <CalendarMonthIcon />
+              <Typography
+                gutterBottom
+                marginLeft={1}
+                marginRight={2}
+                variant="subtitle1"
+              >{`${
+                date.getMonth() + 1
+              }/${date.getDate()}/${date.getFullYear()}`}</Typography>
+
+              <InboxOutlinedIcon />
+              <Typography variant="subtitle1" marginLeft={1}>
+                {post.category.name}
+              </Typography>
+            </div>
+          </div>
+          <Typography variant="subtitle1">{post.description}</Typography>
         </CardContent>
       </Box>
     </Card>
