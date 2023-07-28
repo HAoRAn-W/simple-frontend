f409f6a8e6f0bd934bb518bc0bed689911b8da2e
Haoran Wang <ryan26us@gmail.com>
2023-07-27 21:22:45 -0400

    re write style

--- src/components/category/CategoryCard.js
+++ src/components/category/CategoryCard.js
@@ -1,18 +1,17 @@
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {Paper, Typography } from "@mui/material";
+import Image from "mui-image";
 import React from "react";
 
 function CategoryCard({ category }) {
   return (
-    <Card>
-      <CardMedia
-        sx={{ height: 140, width: 300 }}
-        image={category.coverUrl}
-        title="green iguana"
-      />
-      <CardContent>
+    <Paper style={{ display: "flex", flexDirection: "column", width: 260, height: 260 }}>
+      <div style={{ flex: 8 }}>
+        <Image src={category.coverUrl} style={{maxHeight: '100%'}}/>
+      </div>
+      <div style={{ flex: 4 }}>
         <Typography variant="h5">{category.name}</Typography>
-      </CardContent>
-    </Card>
+      </div>
+    </Paper>
   );
 }
 
