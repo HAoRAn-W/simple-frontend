abbd9ba75453c2ebd4381a030a9b23d5757aae8d
Haoran Wang <ryan26us@gmail.com>
2023-07-30 21:03:28 -0400

    customized renderer

--- src/components/post/PostMarkdown.js
+++ src/components/post/PostMarkdown.js
@@ -1,25 +1,74 @@
+import { CheckBox } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import Image from "mui-image";
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import remarkGfm from "remark-gfm";
 
-
-
-
 function PostMarkdown({ content }) {
-
   const renderers = {
-    h1: ({ children }) => <Typography variant="h3" fontSize={65} fontFamily={'Roboto'}>{children}</Typography>,
-    h2: ({ children }) => <Typography variant="h3" fontSize={50} fontFamily={'Roboto'}>{children}</Typography>,
-    h3: ({ children }) => <Typography variant="h3" fontSize={40} fontFamily={'Roboto'}>{children}</Typography>,
-    h4: ({ children }) => <Typography variant="h3" fontSize={35} fontFamily={'Roboto'}>{children}</Typography>,
-    h5: ({ children }) => <Typography variant="h3" fontSize={30} fontFamily={'Roboto'}>{children}</Typography>,
-    h6: ({ children }) => <Typography variant="h3" fontSize={25} fontFamily={'Roboto'}>{children}</Typography>,
-    p: ({ node, children }) => <Typography variant="body" fontSize={28} style={{ wordWrap: 'break-word' }} gutterBottom>{children}</Typography>,
+    h1: ({ children }) => (
+      <Typography variant="h3" fontSize={65}>
+        {children}
+      </Typography>
+    ),
+    h2: ({ children }) => (
+      <Typography variant="h3" fontSize={50}>
+        {children}
+      </Typography>
+    ),
+    h3: ({ children }) => (
+      <Typography variant="h3" fontSize={40}>
+        {children}
+      </Typography>
+    ),
+    h4: ({ children }) => (
+      <Typography variant="h3" fontSize={35}>
+        {children}
+      </Typography>
+    ),
+    h5: ({ children }) => (
+      <Typography variant="h3" fontSize={30}>
+        {children}
+      </Typography>
+    ),
+    h6: ({ children }) => (
+      <Typography variant="h3" fontSize={25}>
+        {children}
+      </Typography>
+    ),
+    p: ({ children }) => (
+      <Typography
+        variant="body"
+        fontSize={28}
+        style={{ wordWrap: "break-word" }}
+        gutterBottom
+      >
+        {children}
+      </Typography>
+    ),
+    ul: ({ children }) => <Typography component="ul">{children}</Typography>,
+    ol: ({ children }) => <Typography component="ol">{children}</Typography>,
+    li: ({ children }) => (
+      <Typography
+        component="li"
+        variant="body"
+        fontSize={28}
+        style={{ wordWrap: "break-word" }}
+        gutterBottom
+      >
+        {children}
+      </Typography>
+    ),
     img: ({ node, src, alt, title, ...props }) => {
       return (
-        <div style={{ display: 'flex', flexDirection: 'column',alignItems: 'center'}}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <Image
             src={src}
             alt={alt}
@@ -51,7 +100,9 @@ function PostMarkdown({ content }) {
   };
   return (
     <>
-      <ReactMarkdown components={renderers} remarkPlugins={remarkGfm}>{content}</ReactMarkdown>
+      <ReactMarkdown components={renderers} remarkPlugins={remarkGfm}>
+        {content}
+      </ReactMarkdown>
     </>
   );
 }
