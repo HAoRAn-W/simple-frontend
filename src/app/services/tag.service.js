cd4676d782b7da1f86b80d6e79b17f337fd7a8e8
Haoran Wang <ryan26us@gmail.com>
2023-08-01 10:41:41 -0400

    add interceptor for tag

--- src/app/services/tag.service.js
+++ src/app/services/tag.service.js
@@ -1,10 +1,45 @@
 import axios from "axios";
-import { SUCCESSFUL, UNDEFINED_ERROR } from "../constants/MessageCode";
+import { ACCESS_TOKEN_RENEWED, NEED_RELOGIN, REFRESH_TOKEN_EXPIRED, SUCCESSFUL, UNDEFINED_ERROR } from "../constants/MessageCode";
+import RefreshService from "./refresh.service";
 
 const client = axios.create({
   baseURL: "http://localhost:8080/api/tag/",
   withCredentials: true,
 });
+
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  // access token expired OR requesting unauthorized resources
+  (error) => {
+    const request = error.config; // get request
+    if (request.url === "add" || request.url === "update" || request.url === "delete") {
+      if (error.response.status && error.response.status === 401) {
+        if (!request._retry) {
+          // set _retry flag to prevent multiple retries
+          request._retry = true;
+          // try to refresh access token
+          return RefreshService.refreshToken().then((data) => {
+            if (data.code === ACCESS_TOKEN_RENEWED) {
+              console.log("Successfully refresh access token.");
+              return client(request); // retry original request
+            } else if (data.code === REFRESH_TOKEN_EXPIRED) {
+              console.log("Refresh token expired, need to login");
+              return {
+                data: {
+                  code: NEED_RELOGIN,
+                  message: "Refresh token expired, need to login",
+                },
+              };
+            }
+          });
+        }
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 const addTag = (newTag) => {
   return client
@@ -38,7 +73,7 @@ const updateTag = (newTag) => {
 
 const deleteTag = (id) => {
   return client
-    .get(`delete/${id}`)
+    .get(`delete`, {params: {tagId: id}})
     .then((response) => {
       return response.data;
     })
