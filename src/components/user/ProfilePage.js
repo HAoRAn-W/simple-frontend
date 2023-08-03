f7f20dc384064af21c9678ecb1b168b756f72447
Haoran Wang <ryan26us@gmail.com>
2023-08-02 20:51:43 -0400

    Add preset avatars and placeholder

--- src/components/user/ProfilePage.js
+++ src/components/user/ProfilePage.js
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material";
+import { ButtonBase, Paper, Stack, Typography } from "@mui/material";
 import AuthService from "../../app/services/auth.service";
+import avatars from "../avatar/avatar";
+import Image from "mui-image";
 
 function ProfilePage() {
   const user = AuthService.getUser();
@@ -9,10 +11,25 @@ function ProfilePage() {
   // }, []);
 
   return (
-    <>
+    <div style={{ height: "100vh" }}>
       <Typography variant="h5">Welcome, {user.username}!</Typography>
-      {/* <Typography>{content}</Typography> */}
-    </>
+      <Stack
+        spacing={{ xs: 1, sm: 3 }}
+        direction="row"
+        useFlexGap
+        flexWrap="wrap"
+      >
+        {avatars.map((ava) => (
+          <ButtonBase sx={{borderRadius: '50%'}}>
+            <Image
+              src={ava.img}
+              style={{ width: "150px", height: "150px", borderRadius: "50%" }}
+              alt="avatar"
+            />
+          </ButtonBase>
+        ))}
+      </Stack>
+    </div>
   );
 }
 
