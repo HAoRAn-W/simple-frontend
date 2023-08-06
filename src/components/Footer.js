f50897d711a0968167228e8f4cc8fafbe87ff086
Haoran Wang <ryan26us@gmail.com>
2023-08-05 21:26:36 -0400

    code reformat

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,36 @@
-import { Box, Container, Link, Typography } from '@mui/material';
-import React from 'react'
+import { Box, Container, Link, Typography } from "@mui/material";
+import React from "react";
 
 function Footer() {
-    return (
-        <Box component="footer" sx={{ bgcolor: 'background.paper', py:4, bottom: 0, position: "relative",
-        mt: 5,
-    }}
-       >
-          <Container maxWidth="lg">
-            <Typography variant="h5" align="center" gutterBottom>
-              whr.one
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              color="text.secondary"
-            >
-              The best people in life are free
-            </Typography>
-            <Typography
-              variant="subtitle2"
-              align="center"
-              color="text.secondary"
-            >
-              Built with React.js and MUI
-            </Typography>
-            <Copyright />
-          </Container>
-        </Box>
-      );
+  return (
+    <Box sx={{ bgcolor: "background.paper", mt: 4, mb: 2 }}>
+      <Container maxWidth="lg">
+        <Typography variant="h5" align="center" gutterBottom>
+          whr.one
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="text.secondary">
+          The best people in life are free
+        </Typography>
+        <Typography variant="subtitle2" align="center" color="text.secondary">
+          Built with React.js and MUI
+        </Typography>
+        <Copyright />
+      </Container>
+    </Box>
+  );
 }
 
-
 function Copyright() {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-          whr.one
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        whr.one
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-export default Footer
+export default Footer;
