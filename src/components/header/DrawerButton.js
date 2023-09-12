9f9e445c2423b4719921e41d3066ef2eff2433d2
Haoran Wang <ryan26us@gmail.com>
2023-09-12 19:34:18 -0400

    update drawer style to popover

--- src/components/header/DrawerButton.js
+++ src/components/header/DrawerButton.js
@@ -1,88 +1,48 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import {
-  Box,
-  ButtonBase,
-  Drawer,
-  List,
-  ListItem,
-  Typography,
-} from "@mui/material";
+import { ButtonBase, MenuItem, Popover, Typography } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function DrawerButton() {
   const navigate = useNavigate();
-  const [open, setOpen] = useState(false);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
-  const toggleDrawer = (isOpen) => {
-    setOpen(isOpen);
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
   };
 
   const handleClick = (dest) => {
+    setAnchorElUser(null);
     navigate(`/${dest}`);
-    setOpen(false);
   };
 
   return (
     <>
-      <ButtonBase
-        sx={{ marginRight: 3 }}
-        onClick={() => {
-          toggleDrawer(true);
-        }}
-      >
+      <ButtonBase sx={{ marginRight: 3 }} onClick={handleOpenUserMenu}>
         <FontAwesomeIcon icon={faBars} style={{ height: 25 }} />
       </ButtonBase>
-
-      <Drawer anchor="left" open={open} onClose={() => toggleDrawer(false)}>
-        <Box
-          sx={{ width: "25vw", paddingTop: "2rem" }}
-          onClick={() => toggleDrawer(false)}
-        >
-          <List sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            <ListItem disablePadding sx={{ justifyContent: "center" }}>
-              <ButtonBase disableRipple>
-                <Typography
-                  fontWeight={"bold"}
-                  fontSize={"1.5rem"}
-                  onClick={() => {
-                    handleClick("category");
-                  }}
-                >
-                  CATEGORY
-                </Typography>
-              </ButtonBase>
-            </ListItem>
-            <ListItem disablePadding sx={{ justifyContent: "center" }}>
-              <ButtonBase disableRipple>
-                <Typography
-                  fontWeight={"bold"}
-                  fontSize={"1.5rem"}
-                  onClick={() => {
-                    handleClick("tag");
-                  }}
-                >
-                  TAG
-                </Typography>
-              </ButtonBase>
-            </ListItem>
-            <ListItem disablePadding sx={{ justifyContent: "center" }}>
-              <ButtonBase disableRipple>
-                <Typography
-                  fontWeight={"bold"}
-                  fontSize={"1.5rem"}
-                  onClick={() => {
-                    handleClick("about");
-                  }}
-                >
-                  ABOUT
-                </Typography>
-              </ButtonBase>
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
+      <Popover
+        sx={{ mt: "45px" }}
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        open={Boolean(anchorElUser)}
+        onClose={() => setAnchorElUser(null)}
+      >
+        <MenuItem onClick={() => handleClick("category")}>
+          <Typography textAlign="center">Category</Typography>
+        </MenuItem>
+        <MenuItem onClick={() => handleClick("tag")}>
+          <Typography textAlign="center">Tag</Typography>
+        </MenuItem>
+        <MenuItem onClick={() => handleClick("about")}>
+          <Typography textAlign="center">About</Typography>
+        </MenuItem>
+      </Popover>
     </>
   );
 }
