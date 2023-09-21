c469768d8004ddb681cf7464ea69f6f9acb60d3a
Haoran Wang <ryan26us@gmail.com>
2023-09-21 15:46:22 -0400

    update post page to support mobile display

--- src/components/post/PostHeader.js
+++ src/components/post/PostHeader.js
@@ -1,26 +1,12 @@
-import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import Image from "mui-image";
 import React from "react";
 
 function PostHeader({ post }) {
-  const createdDate = new Date(post.createdTime);
-
   return (
-    <div style={{ marginBottom: "1rem" }}>
-      <Image
-        src={post.coverUrl}
-        style={{ height: "50vh", borderRadius: 20, marginBottom: "1rem" }}
-      />
-      <Typography
-        variant="h2"
-        style={{ wordWrap: "break-word", color: "#343a40" }}
-      >
-        {post.title}
-      </Typography>
-      <Typography variant="h5" gutterBottom>{`${
-        createdDate.getMonth() + 1
-      }/${createdDate.getDate()}/${createdDate.getFullYear()}`}</Typography>
-    </div>
+    <Box style={{ marginBottom: "1rem" }}>
+      <Image src={post.coverUrl} style={{ height: "40vh", borderRadius: 5 }} />
+    </Box>
   );
 }
 
--- src/components/post/PostPage.js
+++ src/components/post/PostPage.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import PostMarkdown from "./PostMarkdown";
-import { Paper } from "@mui/material";
+import { Box, ButtonBase, Paper, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import PostService from "../../app/services/post.service";
-import { SUCCESSFUL } from "../../app/constants/MessageCode";
-import ActionBar from "./ActionBar";
+import { IS_IN_FAVORITE, SUCCESSFUL } from "../../app/constants/MessageCode";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import PostHeader from "./PostHeader";
+import UserServcice from "../../app/services/user.service";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({});
+  const [isFavorite, setIsFavorite] = useState(false);
+  const user = localStorage.getItem("user");
+
+  const createdDate = new Date(post.createdTime);
 
   useEffect(() => {
     PostService.getPost(id).then((data) => {
@@ -17,24 +22,69 @@ function PostPage() {
         setPost(data.post);
       }
     });
+
+    UserServcice.queryFavorite(id).then((data) => {
+      if (data.code === IS_IN_FAVORITE) {
+        setIsFavorite(true);
+      }
+    });
   }, [id]);
 
+  const handleClick = () => {
+    if (isFavorite) {
+      UserServcice.removeFavorite(id);
+      setIsFavorite(false);
+    } else {
+      UserServcice.addFavorite(id);
+      setIsFavorite(true);
+    }
+  };
+
   return (
-    <div>
-      <Paper
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          marginX: "10%",
-          padding: 5,
-        }}
-        elevation={0}
-      >
-        <PostHeader post={post} />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        paddingX: { xs: 3, sm: 0 },
+      }}
+      elevation={0}
+    >
+      <PostHeader post={post} />
+      <Paper sx={{ paddingX: { sm: 2, md: 10 } }} elevation={0}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <Typography
+            variant="h2"
+            style={{ wordWrap: "break-word", color: "#343a40" }}
+          >
+            {post.title}
+          </Typography>
+
+          {user && (
+            <ButtonBase
+              disableRipple
+              onClick={handleClick}
+              sx={{
+                color: isFavorite ? "#f28482" : "grey",
+                backgroundColor: "white",
+                marginLeft: "1rem",
+              }}
+            >
+              <FavoriteIcon fontSize="large" />
+            </ButtonBase>
+          )}
+        </div>
+        <Typography variant="h5">{`${
+          createdDate.getMonth() + 1
+        }/${createdDate.getDate()}/${createdDate.getFullYear()}`}</Typography>
+
         <PostMarkdown content={post.content} />
       </Paper>
-      <ActionBar post={post} />
-    </div>
+    </Box>
   );
 }
 
